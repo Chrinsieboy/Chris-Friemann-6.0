3ecd443e1e1cd474b7e7b136b4bbd47e5ecaa841
Chris Friemann <50674927+Chrinsieboy@users.noreply.github.com>
2023-06-27 22:37:36 +0200

    feat(fetchApi): made a for each loop with capybara api

--- main.js
+++ main.js
@@ -31,10 +31,43 @@ function scrollToCoffee() {
 }
 
 async function fetchApi() {
-    const response = await fetch('https://api.capy.lol/v1/capybara?json=true');
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const response = await fetch('https://api.capy.lol/v1/capybaras?json=true');
+    const content = await response.json();
+    
+    const container = document.getElementById('carousel');
+    
+    // Loop through the api and create a div for each item
+    content.data.forEach(item => {
+        console.log(item);
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+
+        div.className = 'glide__slide';
+
+        img.className = 'project-image';
+        img.src = item.url;
+        img.alt = item.alt;
+        div.appendChild(img);
+        container.appendChild(div);
+    });
+
+    // Initialize the carousel when the api is fetched
+    new Glide('.glide', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 2,
+        focusAt: 'center',
+        keyboard: false,
+        autoplay: 5000,
+        animationDuration: 500,
+        animationTimingFunc: 'ease-in-out',
+        peek: {
+            before: 0,
+            after: 0
+        },
+        gap: -200,
+    }).mount();
+    return;
 }
 
 // Get the api on page load
